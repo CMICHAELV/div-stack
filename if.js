441ba235143ea1a0d8c90b718598ad81a8074168
CMICHAELV <mikec4dmasses@gmail.com>
2023-07-24 22:54:11 -0700

    first commit

---
+++ if.js
@@ -0,0 +1,39 @@
+// write a prpgram for scoring a student 
+// 70-100, scored an A.
+// 50-60, scored a, B,
+// 41-49, scored a, C 
+// 0-40, failed this exams
+
+let score = 100; 
+let student = "";
+
+ 
+if (score >=70  ) {
+    student = "A";
+    console.log(student);
+}
+
+//let score = 60;
+//let student = "";
+
+//if (score >= 50) {
+   // student = "B";
+  //  console.log(student);
+//}
+
+//let score = 49;
+//let student = "";
+
+//if (score >= 41) {
+ //   student = "c";
+ //   console.log(student);
+//}
+     
+
+//let score = 40;
+//let student = "";
+
+//if (score >= 0) {
+  //  student = "failed"
+  //  console.log(student);
+//}
